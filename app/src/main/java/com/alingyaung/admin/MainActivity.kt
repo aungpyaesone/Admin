763f7b624d253118475670e50dc9715d1bf549dd
Admin <admin@innovationnetworktechnology.com>
2023-10-18 15:25:09 +0630

    added splash api

--- app/src/main/java/com/alingyaung/admin/MainActivity.kt
+++ app/src/main/java/com/alingyaung/admin/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.alingyaung.admin.uis.nav_graph.NavRootGraph
@@ -31,6 +32,7 @@ class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             AdminTheme {
              Surface(
